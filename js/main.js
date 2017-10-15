4b7249ca3605bccb0686bb57ac7b84eb35b750d0
GersonLazaro <gersonlazaro@gersonlazaro.com>
2017-10-14 21:12:11 -0500

    Primera versión funcional del js

---
+++ js/main.js
@@ -0,0 +1,104 @@
+var config;
+var langs = new Array();
+var instructions = new Array();
+
+function loadConfig () {
+  var request = new window.XMLHttpRequest();
+  request.open('GET', 'config.json', true);
+  
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      // Configuración leída exitosamente
+      config = JSON.parse(request.responseText);
+      activateSite();
+    } else {
+      // TODO: Manejar error devuelto por el servidor
+    }
+  };
+  
+  request.onerror = function() {
+    // TODO: Manejar error en la peticion
+  };
+  request.send();
+}
+
+function addSelectOption (letter, value) {
+  var select = document.getElementById("select" + letter);
+  var option = document.createElement("option");
+  option.text = value;
+  option.id = value;
+  select.add(option);
+}
+
+function activateSite () {
+  langs = config.languages;
+  instructions = config.instructions;
+  for(var i = 0; i < langs.length; i++) {
+    addSelectOption('X', langs[i]);
+    addSelectOption('Y', langs[i]);
+  }
+}
+
+function instructionFoundInLanguages (languagesAvailable) {
+  var selectX = document.getElementById("selectX");
+  var selectY = document.getElementById("selectY");
+  var found = 0;
+  for(var i = 0; i < languagesAvailable.length; i++) {
+    if (languagesAvailable[i] === langs[selectX.selectedIndex]) found++;
+    if (languagesAvailable[i] === langs[selectY.selectedIndex]) found++;
+  }
+  if(found == 2) return true;
+  return false;
+}
+
+function getExample (filename, lang, node) {
+  var request = new window.XMLHttpRequest();
+  request.open('GET', "langs/" + lang + "/" + filename, true);
+  
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      // Configuración leída exitosamente
+      node.textContent = request.responseText;
+    } else {
+      // TODO: Manejar error devuelto por el servidor
+    }
+  };
+  
+  request.onerror = function() {
+    // TODO: Manejar error en la peticion
+  };
+  request.send();
+}
+
+
+
+function generateComparison () {
+  document.getElementById("comparison").remove();
+  var comparison = document.createElement("div");
+  comparison.id = "comparison";
+  document.getElementById("container").appendChild(comparison);
+  var selectX = document.getElementById("selectX");
+  var selectY = document.getElementById("selectY");
+  for(var i = 0; i < instructions.length; i++) {
+    console.log("Instruccion " + instructions[i].name);
+    if(instructionFoundInLanguages(instructions[i].languages)) {
+      var instruction = document.createElement("div");
+      var title = document.createElement("h3");
+      title.textContent = instructions[i].name;
+      var description = document.createElement("p");
+      description.textContent = instructions[i].description;
+      instruction.appendChild(title);
+      instruction.appendChild(description);
+      var left = document.createElement("div");
+      var right = document.createElement("div");
+      instruction.appendChild(left);
+      instruction.appendChild(right);
+      getExample(instructions[i].filename, langs[selectX.selectedIndex], left);
+      getExample(instructions[i].filename, langs[selectY.selectedIndex], right);
+      comparison.appendChild(instruction);
+    }
+  }
+}
+
+loadConfig();
+document.getElementById("learnButton").addEventListener("click", generateComparison);
